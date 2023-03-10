8c0c1060aeab28c5306bb4702686b0f3dcb9c207
mochyopy <mochyopy23@gmail.com>
2023-03-10 19:20:49 +0700

    belajar tailwind dasar

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    
+    extend: {
+      colors:{
+        primary: '#1a1a1a',
+        secondary: '#60686a',
+        text: '#dfcdc3',
+        btn: '#719192',
+      }
+      
+    },
+  },
+  plugins: [],
+}
